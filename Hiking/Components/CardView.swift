e42fe53a5a66a35d02407b252b83da55bc99bf4c
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-04 16:04:01 -0700

    Header Tittle and Subtitle Text

--- Hiking/Components/CardView.swift
+++ Hiking/Components/CardView.swift
@@ -12,15 +12,32 @@ struct CardView: View {
         
         ZStack {
             CustomBackgroundView()
-            ZStack{
+            VStack() {
                 
-                Circle()
-                    .fill(LinearGradient(colors: [Color(Color.colorIndigoMedium), Color(Color.colorSalmonLight)], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width: 256, height: 256)
-                
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                VStack(alignment: .leading){
+                    
+                    Text("Hiking")
+                        .font(.system(size: 52))
+                        .fontWeight(.bold)
+                        .foregroundStyle(LinearGradient(colors: [.customColorGrayLight, .customColorGrayMedium], startPoint: .top, endPoint: .bottom))
+                    
+                    Text("Have fun and enjoy outdoor activities with friends and family.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customColorGrayMedium)
+                    
+                }
+                .padding(.horizontal, 30)
+                ZStack{
+                    
+                    Circle()
+                        .fill(LinearGradient(colors: [Color(Color.colorIndigoMedium), Color(Color.colorSalmonLight)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
             }
         }
         .frame(width: 320, height: 570)
