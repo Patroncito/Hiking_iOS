aec3cee54968453cc12562189741202578b08c3a
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-04 15:39:49 -0700

    Create Card Component

---
+++ Hiking/Components/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Hiking
+//
+//  Created by Alfonso Patron on 04/01/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ Hiking/Components/CustomBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomBackgroundView.swift
+//  Hiking
+//
+//  Created by Alfonso Patron on 04/01/24.
+//
+
+import SwiftUI
+
+struct CustomBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomBackgroundView()
+}
