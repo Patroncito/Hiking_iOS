8f5ed2bf6db645d7b4fbd3d96207bc3a94279508
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-04 00:25:40 -0700

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Alfonso Patron on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
