0b158e86b2a2352188db59e096c83b54b4959931
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-04 15:03:57 -0700

    Add colors

--- Hiking/Utility/ColorExtension.swift
+++ Hiking/Utility/ColorExtension.swift
@@ -5,4 +5,18 @@
 //  Created by Alfonso Patron on 04/01/24.
 //
 
+
 import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let customColorGrayLight = Color("ColorGrayLight")
+    static let customColorGrayMedium = Color("ColorGrayMedium")
+    static let customColorGreenDark = Color("ColorGreenDark")
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customColorGreenMedium = Color("ColorGreenMedium")
+    static let customColorIndigoMedium = Color("ColorIndigoMedium")
+    static let customcolorSalmonLight = Color("colorSalmonLight")
+    
+}
